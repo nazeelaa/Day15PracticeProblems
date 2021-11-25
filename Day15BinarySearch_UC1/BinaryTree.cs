1d67df70e54f5cd2f59a7193ef3ca2e63737d14a
nazeelaa <92171051+nazeelaa@users.noreply.github.com>
2021-11-25 23:47:22 +0530

    Binary Search UC_1 Pushed

---
+++ Day15BinarySearch_UC1/BinaryTree.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day15BinarySearch_UC1
+{
+    class BinaryTree
+    {
+
+        public Node root = null;
+
+        public void Add(int i)
+        {
+            Node node = new Node();
+            node.data = i;
+            if (root == null)
+            {
+                root = node;//if root is null assign newNode to root
+            }
+            else
+            {
+                Node current = root;
+                Node parent;
+                while (true)
+                {
+                    parent = current;
+                    if (i < current.data)
+                    {
+                        current = current.LeftNode;
+                        if (current == null)
+                        {
+                            parent.LeftNode = node;  //go left
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        current = current.RightNode;
+                        if (current == null)
+                        {
+                            parent.RightNode = node;  //go right
+                            break;
+                        }
+                    }
+
+                }
+            }
+            Console.WriteLine(node.data + " : Data is added");
+        }
+
+        public void TraverseInorder(Node root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            TraverseInorder(root.LeftNode);
+            Console.Write(root.data + " ");
+            TraverseInorder(root.RightNode);
+
+        }
+        public void TraversePreorder(Node root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+            Console.Write(root.data + " ");
+            TraversePreorder(root.LeftNode);
+
+            TraversePreorder(root.RightNode);
+
+        }
+        public void TraversePostorder(Node root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            TraversePostorder(root.LeftNode);
+
+            TraversePostorder(root.RightNode);
+            Console.Write(root.data + " ");
+        }
+    }
+}
---
+++ Day15BinarySearch_UC1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Day15BinarySearch_UC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Tree Program.");
+            BinaryTree binarytree = new BinaryTree();
+
+            binarytree.Add(56);
+            binarytree.Add(30);
+            binarytree.Add(70);
+
+            Console.Write("\n Inorder result : ");
+            binarytree.TraverseInorder(binarytree.root);
+            Console.WriteLine();
+
+            Console.Write("\n Post-order result : ");
+            binarytree.TraversePostorder(binarytree.root);
+            Console.WriteLine();
+
+            Console.Write("\n Pre-order result : ");
+            binarytree.TraversePreorder(binarytree.root);
+            Console.WriteLine();
+        }
+    }
+}
+
